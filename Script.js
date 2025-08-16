dd3befd78b26344ec27299acade5d81a4914ec27
ekpahelaesibhi <aisibhiekpahel@gmail.com>
2025-08-16 20:51:26 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-let hindi = false;
-
-function switchLanguage() {
-  hindi = !hindi;
-  document.querySelectorAll('[id$="-en"]').forEach(el => el.style.display = hindi ? "none" : "block");
-  document.querySelectorAll('[id$="-hi"]').forEach(el => el.style.display = hindi ? "block" : "none");
-  document.getElementById("lang-btn").innerText = hindi ? "English" : "हिन्दी";
-}
